5a123e7c217cce00494700b0e0cd013c3f4dbd18
azzamsa <oktinastuzi@gmail.com>
2017-10-21 13:54:00 +0700

    deleting lisp doc

--- my-elisp/myorg.el
+++ my-elisp/myorg.el
@@ -9,21 +9,17 @@
 (defun now ()
   "Insert string for the current time formatted like '2:34 PM'."
   (interactive)                 ; permit invocation in minibuffer
-  ;(insert (format-time-string "%D %-I:%M %p")))
-  ; i cange the format to 04.11.2016 8:40 Am, before the month was in front.
+  ;;(insert (format-time-string "%D %-I:%M %p")))
+  ;; i cange the format to 04.11.2016 8:40 Am, before the month was in front.
   (insert (format-time-string "%d.%m.%Y %-I:%M %p")))
 
 (defun today ()
-  "Insert string for today's date nicely formatted in American style,
-e.g. Sunday, September 17, 2000."
+  "Insert string for today's date nicely formatted in American style,e.g. Sunday, September 17, 2000."
   (interactive)                 ; permit invocation in minibuffer
   (insert (format-time-string "%A, %B %e, %Y")))
-  
+
 (defun hour ()
-  "Insert string for the current time formatted like '2:34 PM'."
-  (interactive)                 ; permit invocation in minibuffer
-  ;(insert (format-time-string "%D %-I:%M %p")))
-  ; i cange the format to 04.11.2016 8:40 Am, before the month was in front.
+  (interactive)                 
   (insert (format-time-string " %-I:%M %p")))
 
 ;;; my.org ends here 
