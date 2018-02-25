e5464fc3b3a573882cf1f5bfc351e1eff551b3dc
azzamsa <oktinastuzi@gmail.com>
2018-02-25 13:11:41 +0700

    add simple crux from bbatsov

---
+++ myelisp/simple-crux.el
@@ -0,0 +1,19 @@
+;;; Code:
+
+(defun crux-transpose-windows (arg)
+  "Transpose the buffers shown in two windows.
+Prefix ARG determines if the current windows buffer is swapped
+with the next or previous window, and the number of
+transpositions to execute in sequence."
+  (interactive "p")
+  (let ((selector (if (>= arg 0) 'next-window 'previous-window)))
+    (while (/= arg 0)
+      (let ((this-win (window-buffer))
+            (next-win (window-buffer (funcall selector))))
+        (set-window-buffer (selected-window) next-win)
+        (set-window-buffer (funcall selector) this-win)
+        (select-window (funcall selector)))
+      (setq arg (if (cl-plusp arg) (1- arg) (1+ arg))))))
+
+(provide 'simple-crux)
+;;; simple-crux.el ends here
