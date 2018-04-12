d61f76ee6121dfa38f236b56e530a7448778fda8
azzamsa <oktinastuzi@gmail.com>
2018-04-12 19:08:11 +0700

    menu-bar comment

--- init.el
+++ init.el
@@ -97,8 +97,9 @@
 (if (fboundp 'set-scroll-bar-mode)
     (set-scroll-bar-mode nil))
 (scroll-bar-mode -1)
-(menu-bar-mode -1)
 
+;; disable menu-bar. I have <f12> to toggle it
+(menu-bar-mode -1)
 ;; toggle menu-bar visibility
 (global-set-key (kbd "<f12>") 'menu-bar-mode)
 
