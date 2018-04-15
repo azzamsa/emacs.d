95c74c2e4bcfe9bc23d88d13e03db4c5069a79be
azzamsa <oktinastuzi@gmail.com>
2018-04-15 19:14:23 +0700

    add dired peek files by abo-abo

--- modes/aza-dired.el
+++ modes/aza-dired.el
@@ -39,4 +39,40 @@
     (unless (equal buffer (current-buffer))
       (kill-buffer buffer))))
 
+;; little modification to dired-mode that let's you browse through lots of files
+;; (add-hook 'dired-mode-hook
+;;           (lambda()
+;;             (define-key dired-mode-map (kbd "C-o") 'dired-view-current)     ; was dired-display-file
+;;             (define-key dired-mode-map (kbd "n")   'dired-view-next)           ; was dired-next-line
+;;             (define-key dired-mode-map (kbd "p")   'dired-view-previous))) ; was dired-previous-line
+
+(defun dired-view-next ()
+  "Move down one line and view the current file in another window."
+  (interactive)
+  (dired-next-line 1)
+  (dired-view-current))
+
+(defun dired-view-previous ()
+  "Move up one line and view the current file in another window."
+  (interactive)
+  (dired-previous-line 1)
+  (dired-view-current))
+
+(defun dired-view-current ()
+  "View the current file in another window (possibly newly created)."
+  (interactive)
+  (if (not (window-parent))
+      (split-window))                                   ; create a new window if necessary
+  (let ((file (dired-get-file-for-visit))
+        (dbuffer (current-buffer)))
+    (other-window 1)                                          ; switch to the other window
+    (unless (equal dbuffer (current-buffer))                 ; don't kill the dired buffer
+      (if (or view-mode (equal major-mode 'dired-mode))   ; only if in view- or dired-mode
+          (kill-buffer)))                                                    ; ... kill it
+    (let ((filebuffer (get-file-buffer file)))
+      (if filebuffer                              ; does a buffer already look at the file
+          (switch-to-buffer filebuffer)                                    ; simply switch
+        (view-file file))                                                    ; ... view it
+      (other-window -1))))                   ; give the attention back to the dired buffer
+
 (provide 'aza-dired)
