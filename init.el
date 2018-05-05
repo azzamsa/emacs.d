6df856b576209668b925a24bfeeb4e106394f5dd
azzamsa <oktinastuzi@gmail.com>
2018-05-05 16:41:06 +0700

    move function to preface

--- init.el
+++ init.el
@@ -873,17 +873,16 @@
 (use-package elpy
   :ensure t
   :bind ("C-l" . elpy-shell-clear-shell)
-  :config
-  (use-package company-jedi
-    :ensure t)
-  (elpy-enable)
-
+  :preface
   (defun elpy-shell-clear-shell ()
     "Clear the current shell buffer."
     (interactive)
     (with-current-buffer (process-buffer (elpy-shell-get-or-create-process))
       (comint-clear-buffer)))
-
+  :config
+  (use-package company-jedi
+    :ensure t)
+  (elpy-enable)
   (add-hook 'python-mode-hook
             (lambda ()
               (company-mode t)
