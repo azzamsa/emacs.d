bb8aadd81200d90c5914ae6da961ac018794634c
azzamsa <oktinastuzi@gmail.com>
2018-05-17 06:05:12 +0700

    add emacs-lisp mode

---
+++ modules/aza-emacs-lisp.el
@@ -0,0 +1,65 @@
+(defun prelude-recompile-elc-on-save ()
+  "Recompile your elc when saving an elisp file."
+  (add-hook 'after-save-hook
+            (lambda ()
+              (when (and
+                     (string-prefix-p prelude-dir (file-truename buffer-file-name))
+                     (file-exists-p (byte-compile-dest-file buffer-file-name)))
+                (emacs-lisp-byte-compile)))
+            nil
+            t))
+
+(defun prelude-visit-ielm ()
+  "Switch to default `ielm' buffer.
+Start `ielm' if it's not already running."
+  (interactive)
+  (crux-start-or-switch-to 'ielm "*ielm*"))
+
+(define-key emacs-lisp-mode-map (kbd "C-c C-z") 'prelude-visit-ielm)
+(define-key emacs-lisp-mode-map (kbd "C-c C-c") 'eval-defun)
+(define-key emacs-lisp-mode-map (kbd "C-c C-b") 'eval-buffer)
+
+(defun prelude-conditional-emacs-lisp-checker ()
+  "Don't check doc style in Emacs Lisp test files."
+  (let ((file-name (buffer-file-name)))
+    (when (and file-name (string-match-p ".*-tests?\\.el\\'" file-name))
+      (setq-local flycheck-checkers '(emacs-lisp)))))
+
+
+(defun prelude-emacs-lisp-mode-defaults ()
+  "Sensible defaults for `emacs-lisp-mode'."
+  (smartparens-strict-mode +1)
+  (rainbow-delimiters-mode +1)
+  (eldoc-mode +1)
+  (prelude-recompile-elc-on-save)
+  (rainbow-mode +1)
+  (setq mode-name "EL")
+  (prelude-conditional-emacs-lisp-checker))
+
+(setq prelude-emacs-lisp-mode-hook 'prelude-emacs-lisp-mode-defaults)
+
+
+(add-hook
+ 'emacs-lisp-mode-hook (lambda ()
+                         (run-hooks 'prelude-emacs-lisp-mode-hook))))
+
+
+
+;; ielm is an interactive Emacs Lisp shell
+(defun prelude-ielm-mode-defaults ()
+  "Sensible defaults for `ielm'."
+  (run-hooks 'prelude-interactive-lisp-coding-hook)
+  (eldoc-mode +1))
+
+(setq prelude-ielm-mode-hook 'prelude-ielm-mode-defaults)
+
+(add-hook 'ielm-mode-hook (lambda ()
+                            (run-hooks 'prelude-ielm-mode-hook)))
+(eval-after-load "elisp-slime-nav"
+  '(diminish 'elisp-slime-nav-mode))
+(eval-after-load "rainbow-mode"
+  '(diminish 'rainbow-mode))
+(eval-after-load "eldoc"
+  '(diminish 'eldoc-mode))
+
+(provide 'aza-emacs-lisp)
