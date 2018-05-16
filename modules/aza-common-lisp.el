108ee844e96e84b907d21e80f68788a7d1b927b1
azzamsa <oktinastuzi@gmail.com>
2018-05-17 05:31:15 +0700

    tidy up lisp mode

--- modules/aza-common-lisp.el
+++ modules/aza-common-lisp.el
@@ -1,4 +1,3 @@
-;; TODO check Common Lisp files
 (use-package lisp-mode
   :defer t
   :mode ("\\.cl\\'"
@@ -6,25 +5,27 @@
   :config
   (add-hook 'lisp-mode-hook
             (lambda ()
-              (slime-mode t)
-              (rainbow-delimiters-mode t)
-              (show-paren-mode t)
+              (rainbow-delimiters-mode +1)
               (smartparens-strict-mode +1)
-              (prettify-symbols-mode t))))
+              (prettify-symbols-mode +1))))
 
-;; TODO check Common Lisp files
 (use-package slime
   :ensure t
   :defer t
+  :bind (:map slime-mode-map
+              ("C-c C-s" . slime-selector))
   :config
+  (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol
+        slime-fuzzy-completion-in-place t
+        slime-enable-evaluate-in-emacs t
+        slime-autodoc-use-multiline-p t
+        slime-auto-start 'always)
+  (setq inferior-lisp-program (executable-find "sbcl")
+        slime-contribs '(slime-company slime-fancy slime-cl-indent))
   (add-hook 'slime-repl-mode-hook
             (lambda ()
-              (visual-line-mode 1)
-              (rainbow-delimiters-mode 1)
-              (show-paren-mode 1)))
-  (setq inferior-lisp-program (executable-find "sbcl")
-        slime-contribs '(slime-company slime-fancy)
-        slime-net-coding-system 'utf-8-unix))
+              (smartparens-strict-mode +1)
+              (whitespace-mode -1))))
 
 (use-package slime-company
   :ensure t
