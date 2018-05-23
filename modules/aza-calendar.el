428898a2fd01edeaaddc3ce8c33799155be9eea5
azzamsa <oktinastuzi@gmail.com>
2018-05-23 07:51:41 +0700

    integrate custom-face for calfw

--- modules/aza-calendar.el
+++ modules/aza-calendar.el
@@ -33,6 +33,25 @@
         holiday-bahai-holidays nil
         holiday-hebrew-holidays nil
         holiday-islamic-holidays nil
-        holiday-oriental-holidays nil))
+        holiday-oriental-holidays nil)
+  :custom-face
+  (cfw:face-annotation ((t :foreground "#ffffff" :inherit cfw:face-day-title)))
+  (cfw:face-day-title ((t :background "grey10")))
+  (cfw:face-default-content ((t :foreground "#ffffff")))
+  (cfw:face-default-day ((t :foreground "#b4eeb4" :weight bold :inherit cfw:face-day-title)))
+  (cfw:face-disable ((t :foreground "DarkGray" :inherit cfw:face-day-title)))
+  (cfw:face-grid ((t :foreground "#BADEAC")))
+  (cfw:face-header ((t (:foreground "#d0bf8f" :weight bold))))
+  (cfw:face-holiday ((t :background "grey10" :foreground "#8c5353" :weight bold)))
+  (cfw:face-periods ((t :foreground "#ffe259")))
+  (cfw:face-saturday ((t :foreground "8cd0d3" :weight bold)))
+  (cfw:face-select ((t :background "#2f2f2f")))
+  (cfw:face-sunday ((t :foreground "#cc9393" :weight bold)))
+  (cfw:face-title ((t (:foreground "#f0dfaf" :weight bold :height 2.0 :inherit variable-pitch))))
+  (cfw:face-today ((t :background: "grey10" :weight bold)))
+  (cfw:face-today-title ((t :background "#7f9f7f" :weight bold)))
+  (cfw:face-toolbar ((t :foreground "Steelblue4" :background "#3F3F3F")))
+  (cfw:face-toolbar-button-off ((t :foreground "#f5f5f5" :weight bold)))
+  (cfw:face-toolbar-button-on ((t :foreground "#ffffff" :weight bold))))
 
 (provide 'aza-calendar)
