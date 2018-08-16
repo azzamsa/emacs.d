ad4b35e3556b989f5c930635c12145c1f2a1072e
azzamsa <oktinastuzi@gmail.com>
2018-08-16 10:18:58 +0700

    Drop: use only one shell for shell-pop

--- modules/aza-dired.el
+++ modules/aza-dired.el
@@ -9,7 +9,7 @@
                ("n" . dired-view-next)
                ("p" . dired-view-previous)
                ("s" . xah-dired-sort))
-         ("C-t" . shell-pop-shell))
+         ("C-t" . shell-pop))
          ;;("C-z" . shell-pop-shell))
   :init
   (require 'aza-dired-ext)
