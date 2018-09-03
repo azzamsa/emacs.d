a19ca3341060712ebfd36727a4358c4c6da527aa
azzamsa <oktinastuzi@gmail.com>
2018-09-03 09:07:14 +0700

    Fix: tidy up rust mode

--- modules/aza-rust.el
+++ modules/aza-rust.el
@@ -1,16 +1,21 @@
 (use-package rust-mode
   :ensure t
-  :defer t)
+  :defer t
+  :config
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'rust-mode-hook #'cargo-minor-mode))
 
 (use-package racer
+  :after rust
   :ensure t
   :defer t
   :config
   (setq racer-cmd "~/.cargo/bin/racer")
   (setq racer-rust-src-path (expand-file-name (getenv "RUST_SRC_PATH")))
   (add-hook 'racer-mode-hook #'company-mode)
-  (add-hook 'rust-mode-hook #'racer-mode)
   (add-hook 'racer-mode-hook #'eldoc-mode))
+
+(setq rust-format-on-save t)
 
 (use-package flycheck-rust
   :ensure t
