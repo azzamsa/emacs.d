76f456f31c4bece39d716de52aa0a504428d0820
azzamsa <oktinastuzi@gmail.com>
2018-09-03 09:07:40 +0700

    Add: reftex in latex-mode

--- modules/aza-latex.el
+++ modules/aza-latex.el
@@ -17,13 +17,22 @@
     (setq-default TeX-PDF-mode t) ; output to pdf
     ;; Activate nice interface between RefTeX and AUCTeX
     (setq reftex-plug-into-AUCTeX t)
-    (add-to-list 'TeX-command-list
-                 '("XeLaTeX" "xelatex -interaction=nonstopmode %s"
-                   TeX-run-command t t :help "Run xelatex") t)
+    ;; (add-to-list 'TeX-command-list
+    ;;              '("XeLaTeX" "xelatex -interaction=nonstopmode %s"
+    ;;                TeX-run-command t t :help "Run xelatex") t)
     (add-hook 'LaTeX-mode-hook
               (lambda ()
                 (yas-minor-mode t)
-                (turn-on-auto-fill)
-                (turn-on-reftex)))))
+                (turn-on-auto-fill)))))
+
+(use-package reftex
+  :ensure t
+  :after tex
+  :bind (:map reftex-mode-map
+               ("C-c r r" . reftex-query-replace-document)
+               ("C-c r g" . reftex-grep-document))
+  :config
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t))
 
 (provide 'aza-latex)
