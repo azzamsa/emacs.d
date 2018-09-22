bd8d16f322b76761840be7263ba3417019501b6a
azzamsa <oktinastuzi@gmail.com>
2018-09-22 21:47:45 +0700

    Add: print how many buffers get killed

--- aza-packages/aza-scripts.el
+++ aza-packages/aza-scripts.el
@@ -1,12 +1,18 @@
+(require 'cl-lib)
+
 ;;;###autoload
 (defun aza-kill-other-buffers ()
-  "Kill all buffers but current buffer and special buffers"
+  "Kill all buffers but current buffer and special buffers.
+(Buffer that start with '*' and white space ignored)"
   (interactive)
-  (dolist (buffer (delq (current-buffer) (buffer-list)))
-    (let ((name (buffer-name buffer)))
-      (when (and name (not (string-equal name ""))
-                 (/= (aref name 0) ?\s)
-                 (string-match "^[^\*]" name))
-        (funcall 'kill-buffer buffer)))))
+  (let ((killed-bufs 0))
+    (dolist (buffer (delq (current-buffer) (buffer-list)))
+      (let ((name (buffer-name buffer)))
+        (when (and name (not (string-equal name ""))
+                   (/= (aref name 0) ?\s)
+                   (string-match "^[^\*]" name))
+          (cl-incf killed-bufs)
+          (funcall 'kill-buffer buffer))))
+    (message "Killed %d buffer(s)" killed-bufs)))
 
 (provide 'aza-scripts)
