721730a9f7dd958da564196b30b527775e1a270c
azzamsa <oktinastuzi@gmail.com>
2018-10-09 00:27:25 +0700

    Add many dired addition

--- modules/aza-dired.el
+++ modules/aza-dired.el
@@ -2,25 +2,26 @@
 
 (use-package dired
   :ensure nil
+  :ensure-system-package thunar
   :bind ((:map dired-mode-map
                ("/" . ora-dired-up-directory)
                ("C-r" . ora-dired-rsync)
                ("C-o" . dired-view-current)
                ("n" . dired-view-next)
                ("p" . dired-view-previous)
-               ("s" . xah-dired-sort))
+               ("s" . xah-dired-sort)
+               ("s-o" . dired-open-directory-in-thunar))
          ("C-t" . shell-pop))
   :init
   (require 'aza-dired-ext)
   (require 'dired-x) ; dired-jump is cool
   :config
+
   (use-package dired+
     :load-path "~/.emacs.d/elisp/diredp/"
     :config
     (diredp-toggle-find-file-reuse-dir 1))
-  (use-package wdired
-    :config
-    (setq wdired-use-dired-vertical-movement 'sometimes))
+
   ;; dired - reuse current buffer by pressing 'a'
   (put 'dired-find-alternate-file 'disabled nil)
 
@@ -44,5 +45,29 @@
                 (regexp-opt
                  '("compile_commands.json"
                    "__pycache__")))))
+
+(use-package wdired
+  :after dired
+  :config
+  (setq wdired-use-dired-vertical-movement 'sometimes))
+
+(use-package dired-ranger
+  :ensure t
+  :bind (:map dired-mode-map
+              ("W" . dired-ranger-copy)
+              ("X" . dired-ranger-move)
+              ("Y" . dired-ranger-paste)))
+
+(use-package dired-subtree
+  :config
+  (bind-keys :map dired-mode-map
+             ("i" . dired-subtree-insert)
+             (";" . dired-subtree-remove)))
+
+
+(use-package dired-narrow
+  :ensure t
+  :bind (:map dired-mode-map
+              ("\\" . dired-narrow)))
 
 (provide 'aza-dired)
