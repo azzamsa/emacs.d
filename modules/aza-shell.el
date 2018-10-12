2383172ec1149890f8af0e08f3d0965d8bc50789
azzamsa <oktinastuzi@gmail.com>
2018-10-12 13:58:06 +0700

    Add read-only comint prompt

--- modules/aza-shell.el
+++ modules/aza-shell.el
@@ -23,6 +23,8 @@
     (eshell-send-input)
     (goto-char (point-max)))
 
+  (setq comint-prompt-read-only t)
+
   ;; don't create new frame for helm-eshell-history
   (setq helm-show-completion-display-function
         #'helm-show-completion-default-display-function)
