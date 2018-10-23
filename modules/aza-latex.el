3a8c42d84e2e2e6c610b2129cd580de203e16777
azzamsa <oktinastuzi@gmail.com>
2018-10-23 07:32:51 +0700

    Drop comment on latex-mode

--- modules/aza-latex.el
+++ modules/aza-latex.el
@@ -12,14 +12,10 @@
   (progn
     (setq LaTeX-verbatim-environments
           '("verbatim" "Verbatim" "lstlisting" "minted"))
-    ;;(setq TeX-parse-self t) ; Enable parse on load.
     (setq TeX-auto-save t) ; Enable parse on save.
     (setq-default TeX-PDF-mode t) ; output to pdf
     ;; Activate nice interface between RefTeX and AUCTeX
     (setq reftex-plug-into-AUCTeX t)
-    ;; (add-to-list 'TeX-command-list
-    ;;              '("XeLaTeX" "xelatex -interaction=nonstopmode %s"
-    ;;                TeX-run-command t t :help "Run xelatex") t)
     (add-hook 'LaTeX-mode-hook
               (lambda ()
                 (yas-minor-mode t)
