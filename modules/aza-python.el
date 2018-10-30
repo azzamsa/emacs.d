6291b5e3080a723d1774ff2899e3014c6131db4f
azzamsa <oktinastuzi@gmail.com>
2018-10-30 12:08:58 +0700

    Refactor elpy to be deferred beautifully

--- modules/aza-python.el
+++ modules/aza-python.el
@@ -1,3 +1,23 @@
+(use-package elpy
+  :ensure t
+  :init (with-eval-after-load 'python (elpy-enable))
+  :commands elpy-enable
+  :bind (:map elpy-mode-map
+              ("C-c C-y l" . elpy-shell-clear-shell))
+  :config
+  (defun elpy-shell-clear-shell ()
+    "Clear the current shell buffer."
+    (interactive)
+    (with-current-buffer (process-buffer (elpy-shell-get-or-create-process))
+      (comint-clear-buffer)))
+
+  ;; Use Flycheck instead of Flymake
+  (when (require 'flycheck nil t)
+    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+    (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+  (setq elpy-rpc-backend "jedi"))
+
 (use-package virtualenvwrapper
   :ensure t
   ;; demanded by eshell prompt
@@ -16,24 +36,5 @@
   :init
   (add-hook 'python-mode-hook 'my/python-mode-hook))
 
-(use-package elpy
-  :ensure t
-  :mode ("\\.py\\'" . python-mode)
-  :bind (:map elpy-mode-map
-              ("C-c C-y l" . elpy-shell-clear-shell))
-  :config
-  (defun elpy-shell-clear-shell ()
-    "Clear the current shell buffer."
-    (interactive)
-    (with-current-buffer (process-buffer (elpy-shell-get-or-create-process))
-      (comint-clear-buffer)))
-
-  ;; Use Flycheck instead of Flymake
-  (when (require 'flycheck nil t)
-    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-    (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-  (elpy-enable)
-  (setq elpy-rpc-backend "jedi"))
 
 (provide 'aza-python)
