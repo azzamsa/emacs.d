e062c19d663be924cc8146c9bc470b9213b10ec7
azzamsa <oktinastuzi@gmail.com>
2018-11-19 20:25:35 +0700

    Fix license. Use separate file

--- init.el
+++ init.el
@@ -1,37 +1,3 @@
-;; init.el --- Azzamsa  Emacs configuration
-;;
-;; Copyright (c) 2016 Azzamsa
-;;
-;; Author: Azzamsa <me at azzamsa dot com>
-;; https://github.com/azzamsa/emacs.d
-;; Keywords: convenience
-
-;; This file is not part of GNU Emacs.
-
-;;; Commentary:
-
-;; This is my personal Emacs configuration.  Nothing more, nothing less.
-
-;;; License:
-
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 3
-;; of the License, or (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
-
-
-;;; Code:
-
 ;; Initialize the package system.
 (require 'package)
 
