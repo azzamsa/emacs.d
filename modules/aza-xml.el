ea1b12bb86d2194a36ce98a67ac0b2b78683c8ca
azzamsa <oktinastuzi@gmail.com>
2018-11-25 18:37:44 +0700

    Fix slow emacs when opening big xml

--- modules/aza-xml.el
+++ modules/aza-xml.el
@@ -6,6 +6,8 @@
   (setq nxml-attribute-indent 4)
   (setq nxml-auto-insert-xml-declaration-flag nil)
   (setq nxml-bind-meta-tab-to-complete-flag t)
-  (setq nxml-slash-auto-complete-flag t))
+  (setq nxml-slash-auto-complete-flag t)
+  (add-hook 'nxml-mode-hook (lambda () (rng-validate-mode 0)) 'append)
+  (add-hook 'dix-mode-hook (lambda () (flycheck-mode 0)) 'append))
 
 (provide 'aza-xml)
