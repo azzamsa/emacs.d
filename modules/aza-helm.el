792b527d7aa06ad65d92f7115b8144cae66e72dd
azzamsa <oktinastuzi@gmail.com>
2018-12-06 05:50:36 +0700

    Change list-buffer to helm-mini
    Both have similar key. I type to fast.

--- modules/aza-helm.el
+++ modules/aza-helm.el
@@ -3,6 +3,7 @@
   :bind (("M-x" . helm-M-x)
          ("C-x C-m" . helm-M-x)
          ("C-x b" . helm-mini)
+         ("C-x C-b" . helm-mini)
          ("C-x C-f" . helm-find-files)
          ("C-c h o" . helm-occur)
          ("C-c h s" . helm-swoop)
