991a20b279467a52757c7391be87018701a10b2f
azzamsa <oktinastuzi@gmail.com>
2018-12-23 12:08:21 +0700

    Fix dired-guess-shell-alist-user. Use mpv --force-window

--- modules/aza-dired-ext.el
+++ modules/aza-dired-ext.el
@@ -87,7 +87,7 @@ Version 2015-07-30"
         ("\\.\\(?:xcf\\)\\'" "gimp")
         ("\\.\\(?:mp4\\|mkv\\|avi\\|flv\\|ogv\\|ifo\\|m4v\\|wmv\\|m4a\\|webm\\)\\(?:\\.part\\)?\\'"
          "mpv --force-window")
-        ("\\.\\(?:mp3\\|flac\\|wv\\)\\'" "mpv")
+        ("\\.\\(?:mp3\\|flac\\|wv\\)\\'" "mpv --force-window")
         ("\\.html?\\'" "firefox")
         ("\\.mm?\\'" "freeplane")
         ("\\.\\(?:pptx?\\|odt\\|xlsx?\\|docx?\\)\\'" "libreoffice")
