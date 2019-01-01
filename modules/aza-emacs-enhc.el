be8bf29b73af09502a4cc954584e98661cd5c582
azzamsa <azzam@azzamsa.com>
2019-01-01 23:46:24 +0700

    Add Emacs enhancement

---
+++ modules/aza-emacs-enhc.el
@@ -0,0 +1,22 @@
+(defun crontab-e ()
+  (interactive)
+  (with-editor-async-shell-command "crontab -e"))
+
+(defun prelude-ddg ()
+  "Googles a query or region if any. Thanks Prelude."
+  (interactive)
+  (browse-url
+   (concat
+    "https://duckduckgo.com/?t=lm&q="
+    (if mark-active
+        (buffer-substring (region-beginning) (region-end))
+      (read-string "Search DuckDuckGo: ")))))
+
+(defun unfill-paragraph ()
+  "Takes a multi-line paragraph and makes it into a single line
+of text. By Stefan Monnier"
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+
+(provide 'aza-emacs-enhc)
