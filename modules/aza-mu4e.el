a29a525e811836a2b871232edc450e5042c280d0
azzamsa <azzam@azzamsa.com>
2019-01-01 17:50:22 +0700

    Refactor mu4e config
    - Add some actions
    - Remove unused config

--- modules/aza-mu4e.el
+++ modules/aza-mu4e.el
@@ -1,6 +1,10 @@
 (use-package mu4e
   :ensure nil
   :ensure-system-package mu
+  :preface
+  (defun my-mu4e-compose-hook ()
+    (set-fill-column 72)
+    (flyspell-mode))
   :custom
   (mu4e-change-filenames-when-moving t)
   (mu4e-confirm-quit nil)
@@ -10,22 +14,24 @@
   (mu4e-maildir "~/Email/memail/")
   (mu4e-maildir-shortcuts
    '(("/INBOX" . ?i)
-     ("/All Mail" . ?a)
-     ("/Drafts" . ?D)
+     ("/Drafts" . ?d)
      ("/Sent" . ?s)
-     ("/Trash" . ?T)))
+     ("/Trash" . ?t)))
   (mu4e-refile-folder "/Archive")
   (mu4e-sent-folder "/Sent")
   (mu4e-trash-folder "/Trash")
   (mu4e-drafts-folder "/Drafts")
   (mu4e-attachment-dir "~/Email/memail/Attachments/")
+  (mu4e-html2text-command "w3m -T text/html")
+  (mu4e-org-contacts-file "~/Email/contacts.org")
   (mu4e-update-interval 300)
   (mu4e-use-fancy-chars t)
   (mu4e-view-show-addresses t)
   (mu4e-view-show-images t)
   :config
-  (add-to-list 'mu4e-headers-actions '("org-contact-add" . mu4e-action-add-org-contact) t)
-  (add-to-list 'mu4e-view-actions '("org-contact-add" . mu4e-action-add-org-contact) t))
+  (add-to-list 'mu4e-view-actions '("Add to contact " . mu4e-action-add-org-contact) t)
+  (add-to-list 'mu4e-view-actions '("View in browser" . mu4e-action-view-in-browser) t)
+  (add-hook 'mu4e-compose-mode-hook #'my-mu4e-compose-hook))
 
 (use-package org-mu4e
   :ensure nil
@@ -42,7 +48,6 @@
 (use-package message
   :ensure nil
   :after mu4e
-
   :custom (send-mail-function 'smtpmail-send-it))
 
 (use-package smtpmail
