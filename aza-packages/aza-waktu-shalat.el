315cbf2e6712f2ec60af238f04ed0ac2d69c601c
azzamsa <azzam@azzamsa.com>
2019-01-15 09:45:30 +0700

    :sparkles: Add waktu-tunggu

--- aza-packages/aza-waktu-shalat.el
+++ aza-packages/aza-waktu-shalat.el
@@ -20,6 +20,18 @@
   "Alias list for tipe shalat. User can input single letter
   instead of full tipe shalat name")
 
+(defun waktu-tunggu (goal-time)
+  "Return Hour and Minutes of remaining time against shalat time"
+  (let* ((curr-time (split-string (format-time-string "%H %M" (current-time))))
+         (curr-hour-in-sec (* (string-to-number (nth 0 curr-time)) 3600))
+         (curr-minute-in-sec (* (string-to-number (nth 1 curr-time)) 60))
+         (curr-time-in-sec (+ curr-hour-in-sec curr-minute-in-sec))
+         (goal-hour-in-sec (* (nth 2 (parse-time-string goal-time)) 3600))
+         (goal-minute-in-sec (* (nth 1 (parse-time-string goal-time)) 60))
+         (goal-in-sec (+ goal-hour-in-sec goal-minute-in-sec))
+         (remaining-sec (- goal-in-sec curr-time-in-sec)))
+    (format-seconds "%H %M" remaining-sec)))
+
 ;;;###autoload
 (defun waktu-shalat ()
   "Show waktu shalat via alert. Accept user input defined in
@@ -31,7 +43,8 @@
     (if (eq siswadi-response 404)
         (message "Waktu shalat request failed")
       (let ((waktu (assoc (intern tipe-shalat) (nth 0 siswadi-response))))
-        (alert (format "Waktu shalat %s jam %s" (car waktu) (cdr waktu))
+        (alert (format "\nWaktu shalat %s jam %s\nWaktu tunggu %s" (car waktu)
+                       (cdr waktu) (waktu-tunggu (cdr waktu)))
                :title "Waktu Shalat")))))
 
 (provide 'aza-waktu-shalat)
