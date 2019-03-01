7049d8bc594479258851699add0e836931bcab0c
azzamsa <azzam@azzamsa.com>
2019-03-01 20:58:08 +0700

    Use black instead of yapf

--- modules/aza-python.el
+++ modules/aza-python.el
@@ -3,7 +3,7 @@
   :init (with-eval-after-load 'python (elpy-enable))
   :commands elpy-enable
   :bind ((:map elpy-refactor-map
-               ("f" . elpy-yapf-fix-code))
+               ("f" . elpy-black-fix-code))
          (:map inferior-python-mode-map
                ("C-c C-l" . helm-comint-input-ring)))
   :config
@@ -13,6 +13,12 @@
     (add-hook 'elpy-mode-hook 'flycheck-mode))
 
   (setq elpy-rpc-backend "jedi")
+  (setq whitespace-line-column 90)
+
+  (delight 'python-mode " Py")
+  (delight 'highlight-indentation-mode "")
+  (delight 'subword-mode "")
+  (add-hook 'before-save-hook #'elpy-black-fix-code nil 'make-it-local)
   (add-hook 'elpy-mode-hook (lambda ()
                               (subword-mode +1))))
 
@@ -22,4 +28,6 @@
   (add-hook 'pyvenv-post-activate-hooks (lambda ()
                                           (revert-buffer t t))))
 
+(add-hook 'inferior-python-mode-hook (lambda ()
+                                       (smartparens-mode 1)))
 (provide 'aza-python)
