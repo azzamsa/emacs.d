0833807fd39db7cc420fe7012be0215a8cba8b18
azzamsa <azzam@azzamsa.com>
2019-03-02 09:02:47 +0700

    Go-mode without exec-path package

--- modules/aza-go.el
+++ modules/aza-go.el
@@ -18,8 +18,12 @@
     (set (make-local-variable 'company-backends) '(company-go))
     (go-eldoc-setup)
     (subword-mode +1))
-
   :config
+
+  (setenv "PATH" (concat "/usr/local/go/bin:"
+                         (getenv "PATH")))
+  (add-to-list 'exec-path "/usr/local/go/bin")
+
   (add-to-list 'completion-ignored-extensions ".test")
   (add-hook 'before-save-hook 'gofmt-before-save nil t)
   (add-hook 'go-mode-hook (lambda ()
