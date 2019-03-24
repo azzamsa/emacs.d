d404441e60d7cf4bc3e8677b351bce0a1d253748
azzamsa <azzam@azzamsa.com>
2019-03-25 03:46:09 +0700

    Add cycle spacing keybinding

--- core/aza-global-keybinding.el
+++ core/aza-global-keybinding.el
@@ -8,6 +8,7 @@
 (define-key minibuffer-local-map (kbd "C-c C-l") 'helm-minibuffer-history)
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "s-j") 'dired-jump)
+(global-set-key [remap just-one-space] 'cycle-spacing)
 
 ;; emacs fix
 (global-set-key (kbd "C-k") 'my-delete-line)
