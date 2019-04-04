f5035ce48689e2f789db63520ba282f97a623a76
azzamsa <azzam@azzamsa.com>
2019-04-04 07:48:14 +0700

    Drop unused helm key

--- modules/aza-helm.el
+++ modules/aza-helm.el
@@ -2,8 +2,6 @@
   :delight helm-mode
   :bind (("M-x" . helm-M-x)
          ("C-x C-m" . helm-M-x)
-         ("C-x b" . helm-mini)
-         ("C-x C-b" . helm-mini)
          ("s-m" . helm-mini)
          ("C-x C-f" . helm-find-files)
          ("C-c h o" . helm-occur)
