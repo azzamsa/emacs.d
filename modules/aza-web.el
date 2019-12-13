113281067400b94d7ae1b67f6e11c5f05fd5b624
azzamsa <oktinastuzi@pm.me>
2019-12-13 17:53:11 +0700

    Use 4 space indent for the web

--- modules/aza-web.el
+++ modules/aza-web.el
@@ -4,26 +4,37 @@
   :init (add-hook 'web-mode-hook
                   (lambda ()
                     (emmet-mode +1)
+                    (subword-mode +1)
                     (smartparens-mode -1)))
   :config
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-enable-auto-quoting nil)
-  :custom
-  (web-mode-attr-indent-offset 2)
-  (web-mode-block-padding 2)
-  (web-mode-css-indent-offset 2)
-  (web-mode-code-indent-offset 2)
-  (web-mode-comment-style 2)
-  (web-mode-enable-current-element-highlight t)
-  (web-mode-markup-indent-offset 2))
+  (setq web-mode-enable-auto-pairing nil)
+
+  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-script-padding 4)
+  (setq web-mode-style-padding 4)
+  (setq web-mode-comment-style 4)
+  (setq web-mode-css-indent-offset 4)
+
+  ;; (setq-default indent-tabs-mode nil)
+  ;; (setq web-mode-enable-current-element-highlight t)
+  ;; (setq web-mode-enable-auto-quoting nil)
+
+  ;; default to django
+  (setq web-mode-set-engine "django"))
 
 (use-package css-mode
-  :mode "\\.css\\'")
+  :mode "\\.css\\'"
+  :config
+  (setq css-indent-level 4)
+  (setq css-indent-offset 4))
 
 (use-package emmet-mode
   :delight
   :bind (:map emmet-mode-keymap
               ("M-e" . emmet-expand-line))
   :hook (css-mode sgml-mode web-mode))
+
+(use-package prettier-js)
 
 (provide 'aza-web)
