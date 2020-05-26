0e302b1abc5f19d791c10df380c88c75b1c14fdf
azzamsa <oktinastuzi@pm.me>
2020-05-26 11:01:31 +0700

    drop unused shell packages

--- modules/aza-shell.el
+++ modules/aza-shell.el
@@ -1,33 +1,7 @@
-(use-package shell
-  :defer t
-  :bind ((:map shell-mode-map
-               ("C-c C-l" . helm-comint-input-ring))
-         ("s-g" . dirs))
-  :config
-  (setq comint-prompt-read-only t) ; make shell-prompt read-only
-  (setq comint-input-ignoredups t))
-
-(use-package shell-here
-  :defer t
-  :after shell
-  :commands shell-here)
-
-(use-package xterm-color
-  :after shell-here
-  :config
-  (setq comint-output-filter-functions
-        (remove 'ansi-color-process-output comint-output-filter-functions))
-
-  (add-hook 'shell-mode-hook
-            (lambda () (add-hook 'comint-preoutput-filter-functions
-                                 'xterm-color-filter nil t))))
-
-(use-package bash-completion
-  :after shell-here
-  :init
-  (bash-completion-setup))
-
 (use-package vterm
+  :straight (vterm :type git :flavor melpa
+                   :files ("*" (:exclude ".dir-locals.el" ".gitignore" ".clang-format" ".travis.yml") "vterm-pkg.el")
+                   :host github :repo "akermu/emacs-libvterm")
   :bind (:map vterm-mode-map
               ("<f2>" . vterm-toggle)
               ([(control return)]  . vterm-toggle-insert-cd)
@@ -38,6 +12,7 @@
   (setq cursor-type 'bar))
 
 (use-package vterm-toggle
-  :straight (vterm-toggle :type git :host github :repo "jixiuf/vterm-toggle"))
+  :after vterm
+  :straight (vterm-toggle :type git :host github :repo "jixiuf/vterm-toggle" :no-build t))
 
 (provide 'aza-shell)
