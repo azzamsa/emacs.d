2e20012205260287c3faa48ab3a27af4415f8d78
azzamsa <azzamsa@protonmail.com>
2020-06-03 06:31:44 +0700

    add helm history in vterm

--- modules/aza-shell.el
+++ modules/aza-shell.el
@@ -5,11 +5,42 @@
   :bind (:map vterm-mode-map
               ("<f2>" . vterm-toggle)
               ([(control return)]  . vterm-toggle-insert-cd)
-              ("C-c C-l" . helm-comint-input-ring)
+              ("C-c C-l" . helm-vterm-history)
               ("C-n" . vterm-toggle-forward)
               ("C-b" . vterm-toggle-backward))
   :config
-  (setq cursor-type 'bar))
+  (setq cursor-type 'bar)
+  (vterm "vterm-main")
+
+  :config
+  (setq term-prompt-regexp "^[^#$%>\\n]*[#$%>] *")
+  (setq vterm-history
+        (expand-file-name "vterm-history" aza-savefile-dir))
+
+  (defun vterm-send-return ()
+    "Send `C-m' to the libvterm."
+    (interactive)
+    (when vterm--term
+      (let* ((beg (vterm--get-prompt-point))
+             (end (vterm--get-end-of-line))
+             (string (buffer-substring-no-properties beg end))
+             (file vterm-history))
+        (if (not (string= string ""))
+          (write-region (concat string "\n") nil file t 0)))
+      (if (vterm--get-icrnl vterm--term)
+          (process-send-string vterm--process "\C-j")
+        (process-send-string vterm--process "\C-m"))))
+
+  (defun helm-vterm-history ()
+    (interactive)
+    (let ((contents (with-temp-buffer
+                      (insert-file-contents vterm-history)
+                      (reverse (split-string (buffer-string) "\n")))))
+      (helm :sources `((name . "vterm history")
+                       (candidates . contents)
+                       (action . (lambda (candidate)
+                                   (vterm-send-string candidate))))
+            :candidate-number-limit 10000))))
 
 (use-package vterm-toggle
   :after vterm
