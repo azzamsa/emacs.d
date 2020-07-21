672b3cbd63d2610ab841e832a36d15a015fe7284
azzamsa <me@azzamsa.com>
2020-07-21 15:09:07 +0700

    fix: drop ercn. always fail in straight build
    erc-notification module is enough for now

--- modules/aza-erc.el
+++ modules/aza-erc.el
@@ -1,7 +1,7 @@
 (use-package erc
-  :defer t
+  :defer 1
   :preface
-  ;; thanks rememberYou
+  ;; @rememberYou
   (defun my/erc-count-users ()
     "Displays the number of users connected on the current channel."
     (interactive)
@@ -14,23 +14,12 @@
             (user-error "The current buffer is not a channel")))
       (user-error "You must first start ERC")))
 
-  (defun my/erc-notify (nickname message)
-    "Displays a notification message for ERC."
-    (let* ((channel (buffer-name))
-           (nick (erc-hl-nicks-trim-irc-nick nickname))
-           (title (if (string-match-p (concat "^" nickname) channel)
-                      nick
-                    (concat nick " (" channel ")")))
-           (msg (s-trim (s-collapse-whitespace message))))
-      (alert (concat nick ": " msg) :title title)))
-
   (defun my/erc-preprocess (string)
     "Avoids channel flooding."
     (setq str
           (string-trim
            (replace-regexp-in-string "\n+" " " str))))
-  :hook ((ercn-notify . my/erc-notify)
-         (erc-send-pre . my/erc-preprocess))
+  :hook (((erc-send-pre . my/erc-preprocess)))
   :config
   (setq erc-hide-list '("PART" "QUIT" "JOIN"))
   (setq erc-autojoin-channels-alist '(("freenode.net"))
@@ -52,19 +41,20 @@
   (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
                                   "324" "329" "332" "333" "353" "477"))
 
+  (setq erc-prompt-for-password nil)
+  (setq erc-prompt-for-nickserv-password nil)
+  (setq erc-autojoin-timing 'ident)
+  (setq erc-server-reconnect-attempts 5)
+  (setq erc-server-reconnect-timeout 3)
+
   (add-to-list 'erc-modules 'notifications)
   (add-to-list 'erc-modules 'spelling)
-  (erc-services-mode +1)
-  ;; exclude boring stuff from tracking
+
+  (erc-track-mode +1)
+  (erc-log-mode +1)
   (erc-truncate-mode +1)
-  (erc-track-mode t)
-  (erc-log-mode)
-  (erc-update-modules)
-  :custom
-  (erc-prompt-for-nickserv-password nil)
-  (erc-autojoin-timing 'ident)
-  (erc-server-reconnect-attempts 5)
-  (erc-server-reconnect-timeout 3))
+  (erc-services-mode +1)
+  (erc-update-modules))
 
 (use-package erc-hl-nicks :after erc)
 
