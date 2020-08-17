b8e4e68fe6fd897f0399c86e2eb1624eb79a94c8
azzamsa <me@azzamsa.com>
2020-08-17 16:13:20 +0700

    refactor: drop keybinding in lsp-ui

--- modules/aza-lsp.el
+++ modules/aza-lsp.el
@@ -9,16 +9,17 @@
   (setq lsp-log-io nil)
   (setq lsp-trace nil)
   (setq lsp-print-performance nil)
-  :custom
-  (lsp-vetur-format-default-formatter-css "none")
-  (lsp-vetur-format-default-formatter-html "none")
-  (lsp-vetur-format-default-formatter-js "none")
-  (lsp-vetur-validation-template nil))
+
+  (setq lsp-vetur-format-default-formatter-css "none")
+  (setq lsp-vetur-format-default-formatter-html "none")
+  (setq lsp-vetur-format-default-formatter-js "none")
+  (setq lsp-vetur-validation-template nil))
 
 (use-package company-lsp
   :after lsp-mode
   :commands lsp-ui-mode
-  :config (push 'company-lsp company-backends))
+  :config
+  (push 'company-lsp company-backends))
 
 (use-package helm-lsp
   :after lsp-mode
@@ -28,17 +29,6 @@
   :after lsp-mode
   :commands lsp-ui-mode
   :config
-  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-  (define-key lsp-ui-mode-map (kbd "C-c C-l .") 'lsp-ui-peek-find-definitions)
-  (define-key lsp-ui-mode-map (kbd "C-c C-l ?") 'lsp-ui-peek-find-references)
-  (define-key lsp-ui-mode-map (kbd "C-c C-l r") 'lsp-rename)
-  (define-key lsp-ui-mode-map (kbd "C-c C-l x") 'lsp-restart-workspace)
-  (define-key lsp-ui-mode-map (kbd "C-c C-l w") 'lsp-ui-peek-find-workspace-symbol)
-  (define-key lsp-ui-mode-map (kbd "C-c C-l i") 'lsp-ui-peek-find-implementation)
-  (define-key lsp-ui-mode-map (kbd "C-c C-l d") 'lsp-describe-thing-at-point)
-  (define-key lsp-ui-mode-map (kbd "C-c C-l e") 'lsp-execute-code-action)
-
   (setq lsp-ui-sideline-enable t)
   (setq lsp-ui-doc-enable t)
   (setq lsp-ui-peek-enable t)
