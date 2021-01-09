2608791416f7e074a36d9bae208b67f65c490762
azzamsa <me@azzamsa.com>
2021-01-09 12:08:10 +0700

    fix: format on save now works well
    Solved by 76a7a25 commit

--- modules/aza-rust.el
+++ modules/aza-rust.el
@@ -2,9 +2,6 @@
   :defer 0.3
   :hook (rustic-mode . lsp)
   :config
-  ;; (setq rustic-format-on-save t)
-  ;; it's very distracting, Now I'm using Makefile
-
   (add-hook 'rust-mode-hook #'subword-mode)
 
   (setq lsp-rust-server 'rust-analyzer)
