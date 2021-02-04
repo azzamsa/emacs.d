64ae05052d5519ac49797aa9dabd7eea401c0957
azzamsa <me@azzamsa.com>
2021-02-04 07:42:02 +0700

    style: format

--- modules/aza-python.el
+++ modules/aza-python.el
@@ -8,7 +8,6 @@
   (setq lsp-pyls-plugins-flake8-enabled t)
   ;; https://github.com/emacs-lsp/lsp-mode/issues/746#issuecomment-480565612
   ;; https://github.com/palantir/python-language-server/issues/190
-  (setq lsp-pyls-configuration-sources ["flake8"])
-  )
+  (setq lsp-pyls-configuration-sources ["flake8"]))
 
 (provide 'aza-python)
