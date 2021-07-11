30133a049372f2e5f1f8a9075c7c31c3ff9905a6
azzamsa <me@azzamsa.com>
2021-07-12 06:51:07 +0700

    feat: revert zoom feature in hydra

--- modules/aza-hydra.el
+++ modules/aza-hydra.el
@@ -10,12 +10,18 @@
   ^^                  _r_ replace
   ^^                  _e_ er
   ^^                  _n_ mc/next
+  ^^                  _+_ in
+  ^^                  _-_ out
+  ^^                  _=_ reset
   ^^                  ^^
   "
   ("q" nil)
   ("z" undo)
   ("r" anzu-query-replace-regexp)
   ("e" er/expand-region)
-  ("n" mc/mark-next-like-this))
+  ("n" mc/mark-next-like-this)
+  ("+" text-scale-increase)
+  ("-" text-scale-decrease)
+  ("=" (text-scale-increase 0)))
 
 (provide 'aza-hydra)
