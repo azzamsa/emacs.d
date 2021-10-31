f449ed0eb13c4bb5ca4ec2b72b953aefa70110f6
azzamsa <me@azzamsa.com>
2021-10-31 10:50:05 +0700

    fix: reconfigure hydra

--- modules/aza-hydra.el
+++ modules/aza-hydra.el
@@ -1,27 +1,30 @@
 (use-package hydra
-  :bind ("<f7>" . hydra-menu/body))
+  :bind (("s-l m" . hydra-menu/body)
+         ("s-l c" . hydra-capital/body)))
 
 (defhydra hydra-menu (:color pink)
   "
-  ^
-  ^Menu^           ^Menu^
-  ^───────^───────────^──────^──────
-  _q_ quit            _z_ undo
-  ^^                  _r_ replace
-  ^^                  _e_ er
-  ^^                  _n_ mc/next
-  ^^                  _+_ in
-  ^^                  _-_ out
-  ^^                  _=_ reset
-  ^^                  ^^
+  _q_uit  _x_ redo     _r_eplace
+  _e_r    _n_ mc/next  _+_ in     _-_ out  _=_ reset
   "
   ("q" nil)
-  ("z" undo)
+  ("z" (undo-only))
+  ("x" (undo-redo))
   ("r" anzu-query-replace-regexp)
   ("e" er/expand-region)
   ("n" mc/mark-next-like-this)
   ("+" text-scale-increase)
   ("-" text-scale-decrease)
   ("=" (text-scale-increase 0)))
+
+(defhydra hydra-capital (:color blue)
+  "
+  _q_uit  _d_owncase  _u_pcase  _c_apital
+  "
+  ("q" nil)
+  ("d" downcase-region)
+  ("u" upcase-region)
+  ("c" capitalize-region))
+
 
 (provide 'aza-hydra)
