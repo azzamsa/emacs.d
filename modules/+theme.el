755a1e6913e5cf2a5b37c62d83a561e700fb0bce
Azzam S.A <noreply@azzamsa.com>
2026-04-28 16:49:57 +0700

    style: add more themes

--- modules/+theme.el
+++ modules/+theme.el
@@ -1,36 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
-(use-package tokyo-night
-  :vc (:url "https://github.com/bbatsov/tokyo-night-emacs" :rev :newest)
-  :config
-  (setq  tokyo-night-override-colors-alist
-         '(
-           ;;
-           ;; Tokyo black
-           ;;
-
-           ;; Background shades
-           ("tokyo-bg-darkest"    . "#000000")
-           ("tokyo-bg-dark"       . "#000000")
-           ("tokyo-bg"            . "#000000")
-
-           ;; Foreground shades
-           ("tokyo-fg"            . "#ffffff")
-           ("tokyo-fg-dark"       . "#c8d3f5")
-           ("tokyo-fg-muted"      . "#828bb8")
-           ("tokyo-fg-gutter"     . "#3b4261")
-
-           ;; UI elements
-           ("tokyo-line-nr"       . "#828bb8")
-           ("tokyo-line-nr-cur"   . "#fca7ea")
-           ("tokyo-selection"     . "#292e42") ; 13.42:1 on white
-
-           ;; brighter comments
-           ("tokyo-comment" . "#9aa5ce")))
-  (load-theme 'tokyo-night-moon t))
-
 (use-package modus-themes
-  :disabled
   :ensure t
   :config
   (setq modus-themes-italic-constructs t
@@ -52,4 +22,78 @@
           (border-mode-line-inactive bg-mode-line-inactive)
           ;; Line numbers
           (bg-line-number-active bg-dim)))
-  (load-theme 'modus-vivendi t))
+  ;; (load-theme 'modus-vivendi t))
+  (load-theme 'modus-vivendi-tinted t))
+
+(use-package batppuccin
+  :disabled
+  :ensure (:host github :repo "bbatsov/batppuccin-emacs")
+  :config
+  (setq batppuccin-override-colors-alist
+        '(("bat-base" . "#000000")
+          ("bat-text" . "#ffffff")))
+  (load-theme 'batppuccin-mocha t))
+
+(use-package tokyo-night
+  :disabled
+  :vc (:url "https://github.com/bbatsov/tokyo-night-emacs" :rev :newest)
+  :config
+  (setq  tokyo-night-override-colors-alist
+         '(
+           ;;
+           ;; Tokyo black
+           ;;
+
+           ;; Background shades
+           ("tokyo-bg-darkest"    . "#0d0e1c")
+           ("tokyo-bg-dark"       . "#1d2235")
+           ("tokyo-bg"            . "#000000")
+
+           ;; Foreground shades
+           ("tokyo-fg"            . "#ffffff")
+           ("tokyo-fg-dark"       . "#c8d3f5")
+           ("tokyo-fg-muted"      . "#828bb8")
+           ("tokyo-fg-gutter"     . "#3b4261")
+
+           ;; UI elements
+           ("tokyo-line-nr"       . "#828bb8")
+           ("tokyo-line-nr-cur"   . "#fca7ea")
+           ("tokyo-selection"     . "#292e42") ; 13.42:1 on white
+
+           ;; brighter comments
+           ("tokyo-comment" . "#9aa5ce")))
+  (load-theme 'tokyo-night-moon t))
+
+(use-package ef-themes
+  :disabled
+  :ensure t
+  :init
+  (ef-themes-take-over-modus-themes-mode 1)
+  :config
+  ;; All customisations here.
+  (setq modus-themes-mixed-fonts t)
+  (setq modus-themes-italic-constructs t)
+
+  ;; (setq ef-winter-palette-overrides '((bg-main "#000000")))
+  ;; (setq ef-dark-palette-overrides '((bg-main "#000000")))
+
+  (modus-themes-load-theme 'ef-winter))
+
+(use-package doom-themes
+  :disabled
+  :ensure t
+  :config
+  (load-theme 'doom-dracula t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (nerd-icons must be installed!)
+  (doom-themes-neotree-config)
+
+  ;; Override background to pure black
+  (custom-set-faces
+   '(default ((t (:background "#000000"))))
+   '(hl-line ((t (:background "#1a1a1a"))))
+   '(line-number ((t (:background "#000000"))))
+   '(line-number-current-line ((t (:background "#1a1a1a"))))
+   '(fringe ((t (:background "#000000"))))))
