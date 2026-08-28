b9795ae9de1dfa04b201a98b6abec554340d308f
Azzam S.A <noreply@azzamsa.com>
2026-08-28 20:39:26 +0700

    feat: try `meow`

---
+++ modules/meow.el
@@ -0,0 +1,103 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package meow
+  :demand t
+  :ensure t
+  :config
+  (defun meow-setup ()
+    (meow-motion-define-key
+     '("t" . meow-prev)
+     '("<escape>" . ignore))
+
+    (meow-leader-define-key
+     '("?" . meow-cheatsheet)
+     '("1" . meow-digit-argument)
+     '("2" . meow-digit-argument)
+     '("3" . meow-digit-argument)
+     '("4" . meow-digit-argument)
+     '("5" . meow-digit-argument)
+     '("6" . meow-digit-argument)
+     '("7" . meow-digit-argument)
+     '("8" . meow-digit-argument)
+     '("9" . meow-digit-argument)
+     '("0" . meow-digit-argument))
+
+    (meow-normal-define-key
+     ;; numbers / arguments
+     '("0" . meow-expand-0)
+     '("1" . meow-expand-1)
+     '("2" . meow-expand-2)
+     '("3" . meow-expand-3)
+     '("4" . meow-expand-4)
+     '("5" . meow-expand-5)
+     '("6" . meow-expand-6)
+     '("7" . meow-expand-7)
+     '("8" . meow-expand-8)
+     '("9" . meow-expand-9)
+     '("-" . negative-argument)
+
+     ;; selection
+     '(";" . meow-reverse)
+     '("," . meow-inner-of-thing)
+     '("." . meow-bounds-of-thing)
+     '("[" . meow-beginning-of-thing)
+     '("]" . meow-end-of-thing)
+     '("/" . meow-visit)
+
+     ;; movement
+     ;;     E
+     ;;     ↑
+     ;; C ←   → I
+     ;;     ↓
+     ;;     A
+     '("c" . meow-left)
+     '("C" . meow-left-expand)
+     '("a" . meow-next)
+     '("A" . meow-next-expand)
+     '("e" . meow-prev)
+     '("E" . meow-prev-expand)
+     '("i" . meow-right)
+     '("I" . meow-right-expand)
+
+     ;; editing
+     '("s" . meow-insert)
+     '("S" . meow-open-above)
+     '("n" . meow-append)
+     '("t" . meow-open-below)
+     '("d" . meow-change)
+     '("x" . meow-delete)
+     '("X" . meow-backward-delete)
+     '("k" . meow-kill)
+     '("p" . meow-yank)
+     '("r" . meow-replace)
+
+     ;; words
+     '("w" . meow-next-word)
+     '("W" . meow-next-symbol)
+     '("b" . meow-back-word)
+     '("B" . meow-back-symbol)
+
+     ;; marks / lines / blocks
+     '("m" . meow-mark-word)
+     '("M" . meow-mark-symbol)
+     '("l" . meow-line)
+     '("L" . meow-goto-line)
+     '("o" . meow-block)
+     '("O" . meow-to-block)
+
+     ;; misc
+     '("f" . meow-find)
+     '("g" . meow-cancel-selection)
+     '("G" . meow-grab)
+     '("j" . meow-join)
+     '("q" . meow-quit)
+     '("u" . meow-undo)
+     '("U" . meow-undo-in-selection)
+     '("v" . meow-search)
+     '("y" . meow-save)
+     '("z" . meow-pop-selection)
+     '("'" . repeat)
+     '("<escape>" . ignore)))
+
+  (meow-setup)
+  (meow-global-mode 1))
